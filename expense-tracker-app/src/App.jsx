998de79d8ce4028d3cb92d5ca4456a9c05cfa001
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-01-23 21:55:22 +0500

    global state made, all components created

--- expense-tracker-app/src/App.jsx
+++ expense-tracker-app/src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css';
 import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
+
+import { GloabalProvider } from './context/GlobalState';
 
 function App() {
   
 
   return (
     <>
-      <div>
+      <GloabalProvider>
         <Header />
-      </div>
+        <div className="container">
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </div>
+      </GloabalProvider>
     </>
   )
 }
